6bb7a7bfd514a6d660505ccd0a73a7331911aaf9
Mihail Selezniov <mihailselezniov@yandex.ru>
2019-12-11 06:03:11 +0300

    +

--- 13/dump_models/zip_f.py
+++ 13/dump_models/zip_f.py
@@ -1,7 +1,7 @@
 import gzip
 
 for f_name in ['LogReg.j', 'KNN7.j']:
-    f_in = open(f_name)
+    f_in = open(f_name, 'rb')
     f_out = gzip.open(f_name + '.gz', 'wb')
     f_out.writelines(f_in)
     f_out.close()
