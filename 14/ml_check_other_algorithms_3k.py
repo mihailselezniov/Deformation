cc584b2d39567d76e36f82499da427e92a73d187
Mihail Selezniov <mihailselezniov@yandex.ru>
2020-02-19 14:24:59 +0300

    +

---
+++ 14/ml_check_other_algorithms_3k.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score, average_precision_score, f1_score
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import LinearSVC, SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.externals import joblib
+from xgboost import XGBClassifier, XGBRegressor
+import collections
+from numpy import genfromtxt
+
+with open('../11/fib_all_data.txt', 'r') as f:
+    data_is_broken = f.readlines()
+data_is_broken = list(map(int, data_is_broken))
+
+X, Y = [], []
+for i, val in enumerate(data_is_broken):
+    Y.extend([i%2]*val)
+
+n = tuple(map(float, range(10)))
+i = 0
+y = []
+a = np.empty((0,8), dtype=np.float64)
+for i0 in n:
+    for i1 in n:
+        for i2 in n:
+            for i3 in n:
+                for i4 in n:
+                    for i5 in n:
+                        for i6 in n:
+                            for i7 in n:
+                                if 0 not in [i4, i5, i6]:
+                                    X.append([i0, i1, i2, i3, i4, i5, i6, i7])
+                                    y.append(Y[i])
+                                i += 1
+    a = np.append(a, np.array(X), axis=0)
+    X = []
+    print(i0)
+    break
+
+
+print('!!!')
+x_test, y_test = a, np.array(y)
+print(x_test.shape, y_test.shape)
+print('all', dict(collections.Counter(y_test)))
+
+
+source_f = '../12/ml_threads/6_1.txt'
+with open(source_f, 'r') as f:
+    threads = f.readlines()
+
+x_train, y_train = [], []
+for t in threads:
+    tr = list(map(float, t.replace('\n', '').split(',')))
+    x_train.append(tr[:-1])
+    y_train.append(tr[-1])
+
+x, y = np.array(x_train), np.array(y_train)
+
+#print(x_train[:100,:].shape)
+
+
+roc_metrics, pr_metrics, f1_metrics = [], [], []
+roc_metric, pr_metric, f1_metric = [], [], []
+
+for cut in [100, 200, 300, 400, 500]:
+
+    print('\n\n\n', '#'*10, cut, '#'*10)
+    x_train, y_train = x[:cut,:], y[:cut]
+
+    def fit_model(model):
+        global roc_metric
+        global pr_metric
+        global f1_metric
+        print('\n', '-'*10, model.__class__.__name__, '-'*10)
+        print(x_test.shape, y_test.shape)
+        print('y_test', dict(collections.Counter(y_test)), 'y_train', dict(collections.Counter(y_train)))
+        # fit model on training data
+        model.fit(x_train, y_train)
+
+        y_pred = model.predict(x_test)
+        print('y_pred', dict(collections.Counter(y_pred)))
+        # make predictions for test data
+        y_pred = [round(value) for value in y_pred]
+        # evaluate predictions
+        accuracy = accuracy_score(y_test, y_pred)
+        print('Accuracy: {}'.format(accuracy))
+
+        cm = confusion_matrix(y_test, y_pred)
+        print('Confusion matrix:\n{}'.format(cm))
+
+        print('Precision, recall and f1-score:')
+        print(classification_report(y_test, y_pred))
+
+        try:
+            roc = roc_auc_score(y_test, y_pred)
+            print('ROC AUC: {}'.format(roc))
+
+            pr = average_precision_score(y_test, y_pred)
+            print('PR AUC: {}'.format(pr))
+
+            roc_metric.append(round(float(roc), 4))
+            pr_metric.append(round(float(pr), 4))
+
+            f1 = f1_score(y_test, y_pred, average=None)
+            f1_metric.append(round(float(f1[0]), 4))
+
+        except Exception as e:
+            print(e)
+
+        print('-'*10, 'End',  model.__class__.__name__, '-'*10)
+
+
+
+    roc_metric, pr_metric, f1_metric = [], [], []
+
+
+    fit_model(XGBClassifier(random_state=42))
+    fit_model(LogisticRegression())
+    fit_model(LinearSVC(random_state=42, tol=1e-5))
+    fit_model(KNeighborsClassifier(n_neighbors=5))
+    fit_model(SGDClassifier(random_state=42))
+    fit_model(BernoulliNB())
+    fit_model(RandomForestClassifier(random_state=42))
+    fit_model(MLPClassifier())
+    fit_model(SVC(random_state=42))
+
+    roc_metrics.append(roc_metric[:])
+    pr_metrics.append(pr_metric[:])
+    f1_metrics.append(f1_metric[:])
+
+
+print('roc_metrics')
+print(roc_metrics)
+print('pr_metrics')
+print(pr_metrics)
+print('f1_metrics')
+print(f1_metrics)
+
+
+
+'''
+
+'''
+
+
+
+
+
+
+
+
+
